160e4a188ca39097afd2853a2788b153ea961562
Nándor Dudás <nandor.dudas@gmail.com>
2025-04-21 02:06:09 +0200

    chore: add ESLint with @antfu config
    - improve code formatting

---
+++ .config/eslint.ts
@@ -0,0 +1,29 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    formatters: true,
+    // typescript: { tsconfigPath: 'tsconfig.node.json' }, // [INFO] very strict
+  },
+  { files: ['**/tsconfig*.json'], rules: { 'jsonc/sort-keys': 'off' } },
+)
+  .override('antfu/stylistic/rules', {
+    rules: {
+      'curly': ['error', 'multi-or-nest', 'consistent'],
+      'style/brace-style': ['error', 'stroustrup', { allowSingleLine: false }],
+      'style/max-len': ['error', { code: 120, ignorePattern: '.*\\n.*', ignoreComments: true, ignoreUrls: true }],
+      'unicorn/numeric-separators-style': ['error', { number: { minimumDigits: 4, groupLength: 3 } }],
+    },
+  })
+  .override('antfu/vue/rules', {
+    rules: {
+      'vue/max-attributes-per-line': ['error', { singleline: 1, multiline: 1 }],
+    },
+  })
+  .override('antfu/typescript/rules', {
+    rules: {
+      'complexity': ['error', 5],
+      'no-restricted-syntax': ['error', 'DebuggerStatement', 'LabeledStatement', 'WithStatement'],
+      'ts/max-params': ['error', { max: 3 }],
+    },
+  })
